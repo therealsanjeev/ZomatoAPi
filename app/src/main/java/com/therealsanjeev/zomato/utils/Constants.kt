5dffcaaa9223fd84c402b86e0790f6924a38e7aa
Prateek <codes.prateek@gmail.com>
2021-01-24 10:37:25 +0530

    Removed API Key

--- app/src/main/java/com/therealsanjeev/zomato/utils/Constants.kt
+++ app/src/main/java/com/therealsanjeev/zomato/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.therealsanjeev.zomato.utils
 class Constants {
     companion object{
         const val BASE_URL = "https://developers.zomato.com/api/v2.1/"
-        const val API_KEY = "c199607e3366edbb8abd7d7c58fa48fc"
+        const val API_KEY = "ENTER API KEY"
     }
 }
